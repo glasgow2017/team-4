92ac1c7ca50c7631f836113b6251c72cc5c8f19c
mmurnin <mmurnin8@gmail.com>
2017-11-04 18:46:55 +0000

    Pocket typing javascript prevention

--- WhoDares/Help.php
+++ WhoDares/Help.php
@@ -26,7 +26,8 @@ and open the template in the editor.
 </nav>
 <div class="form-group sector-dd">
   <label for="sel1">Sector</label>
-  <select class="form-control" id="sel1">
+  <select onchange = "checkform()"class="form-control" id="sectorSelect">
+    <option selected="selected" disabled = "disabled">Choose option</option>
     <option>Army Vet</option>
     <option>Fire service</option>
     <option>Police</option>
@@ -37,7 +38,8 @@ and open the template in the editor.
     <br>
 <div class="form-group sector-dd">
   <label for="sel1">Issue</label>
-  <select class="form-control" id="sel1">
+  <select onchange = "checkform()" class="form-control" id="issueSelect">
+    <option selected="selected" disabled = "disabled">Choose option</option>
     <option>Stress</option>
     <option>Depression</option>
     <option>Loneliness</option>
@@ -45,7 +47,18 @@ and open the template in the editor.
   </select>
 </div>
     <div class ="btn-submithelp">
-        <button type="button" class="btn btn-primary" disabled="disabled">Get Help</button>
+        <button type="button" class="btn btn-primary btn-Help" id ="btn-Help" disabled="disabled">Get Help</button>
     </div>
 </body>
+    <script type="text/javascript" language="javascript">
+        function checkform()
+        {
+            
+            if(document.getElementById("issueSelect").value != "Choose option" && document.getElementById("sectorSelect").value != "Choose option"){
+                document.getElementById("btn-Help").disabled = false;
+            }
+         
+        }
+        
+    </script>
 </html>
